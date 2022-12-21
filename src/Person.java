bd08974bd29d913fd6c76da7b7cb451cac509a03
Reyes815 <reyes.rowen30@gmail.com>
2022-12-21 13:01:44 +0800

    •MILESTONE C: Create Person class and subclasses

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,65 @@
 public class Person {
     // TODO implement Person and its subclasses in other Java files
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Hello,  my  name  is " + this.name + ".";
+    }
+
+    public class Customer extends Person{
+
+        public Customer(String name, int age) {
+            super(name, age);
+        }
+    }
+
+    public class Employee extends Person {
+        int months_worked;
+        double salary;
+        public Employee(String name, int age) {
+            super(name, age);
+        }
+
+        public double thirteenthmonth(){
+
+        }
+    }
+
+    public class Clerk extends Employee {
+
+
+        public Clerk(String name, int age) {
+            super(name, age);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + "How may I help you?";
+        }
+    }
+
+    public class Manager extends Employee {
+
+
+        public Manager(String name, int age) {
+            super(name, age);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+    }
+
+
 }
+
+
